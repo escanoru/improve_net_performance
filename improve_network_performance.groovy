bd0bbce0a539f8807832f2f6ab879c465e34643d
escanoru <yitzo@yandex.com>
2021-05-05 10:09:24 -0600

    Description corrected

--- improve_network_performance.groovy
+++ improve_network_performance.groovy
@@ -1,6 +1,6 @@
 def setDescription() { 
   def item = Jenkins.instance.getItemByFullName(env.JOB_NAME) 
-  item.setDescription("<h5><span style=\"color:green\">This jobs performs some adjustments on the /etc/sysctl.conf file in order to improve the network peformance<ol><li>net.core.wmem_max = 209715200</li><li>net.core.rmem_max = 209715200</li><li>net.ipv4.tcp_rmem = 5242880 10485760 209715200</li><li>net.ipv4.tcp_wmem = 5242880 10485760 209715200</li><li>net.ipv4.tcp_window_scaling = 1</li><li>net.ipv4.tcp_timestamps = 0</li><li>net.ipv4.tcp_sack = 0</li><li>net.ipv4.tcp_dsack = 0</li><li>net.ipv4.tcp_no_metrics_save = 1</li><li>net.core.netdev_max_backlog = 0000</li></ol></span></h5>") 
+  item.setDescription("<h5><span style=\"color:green\">This jobs performs some adjustments on the /etc/sysctl.conf file in order to improve the network peformance<ol><li>net.core.wmem_max = 209715200</li><li>net.core.rmem_max = 209715200</li><li>net.ipv4.tcp_rmem = 5242880 10485760 209715200</li><li>net.ipv4.tcp_wmem = 5242880 10485760 209715200</li><li>net.ipv4.tcp_window_scaling = 1</li><li>net.ipv4.tcp_timestamps = 0</li><li>net.ipv4.tcp_sack = 0</li><li>net.ipv4.tcp_dsack = 0</li><li>net.ipv4.tcp_no_metrics_save = 1</li><li>net.core.netdev_max_backlog = 10000</li></ol></span></h5>") 
   item.save()
   }
 setDescription()
